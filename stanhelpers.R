8a152c55a3997f09450b2572267a6bffd62c96ba
Marco Colombo <m.colombo@ed.ac.uk>
2017-12-19 16:27:20 +0000

    Make posterior.means() work also when the parameter extracted is a scalar.

--- stanhelpers.R
+++ stanhelpers.R
@@ -16,6 +16,7 @@ summarize.params <- function(samples, pars, varnames) { # returns posterior mean
 
 posterior.means <- function(samples, varnames) { # returns posterior means
     x.summary <- summary(As.mcmc.list(object=samples, pars=varnames))$statistics
+    x.summary <- matrix(x.summary, ncol=4, dimnames=dimnames(x.summary))
     return(x.summary[, 1])
 }
 
