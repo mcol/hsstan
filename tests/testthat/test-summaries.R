6a10b711fd38a410528258c249336df118d92c1a
Marco Colombo <m.colombo@ed.ac.uk>
2019-08-07 21:59:15 +0000

    Reorder test-summaries.R to reflect the order of functions in summaries.R.

--- tests/testthat/test-summaries.R
+++ tests/testthat/test-summaries.R
@@ -1,3 +1,29 @@
+test_that("summary.hsstan",
+{
+    expect_error(summary(hs.gauss, prob=c(0.2, 0.8)),
+                 "'prob' must be a single value between 0 and 1")
+    expect_error(summary(hs.gauss, prob=1),
+                 "'prob' must be a single value between 0 and 1")
+
+    out <- summary(hs.gauss)
+    expect_is(out, "matrix")
+    expect_equal(colnames(out),
+                 c("mean", "sd", "2.5%", "97.5%", "n_eff", "Rhat"))
+    expect_equal(nrow(out),
+                 P + 1 + 1) # intercept and extra factor level for X1
+
+    expect_equal(summary(hs.gauss, max.rows=0), out)
+    expect_equal(nrow(summary(hs.gauss, max.rows=5)), 5)
+
+    out <- summary(hs.gauss, sort="n_eff", decreasing=FALSE)
+    expect_true(all(diff(out[, "n_eff"]) > 0))
+})
+
+test_that("print.hsstan",
+{
+    expect_output(print(hs.gauss))
+})
+
 test_that("get.cv.performance works for non-crossvalidated models",
 {
     out <- get.cv.performance(hs.gauss)
@@ -48,32 +74,6 @@ test_that("get.cv.performance works for one fold of a cross-validated model",
                  get.cv.performance(cv.gauss)$test.llk[1])
     expect_equal(out$r2,
                  get.cv.performance(cv.gauss)$r2[1])
-})
-
-test_that("summary.hsstan",
-{
-    expect_error(summary(hs.gauss, prob=c(0.2, 0.8)),
-                 "'prob' must be a single value between 0 and 1")
-    expect_error(summary(hs.gauss, prob=1),
-                 "'prob' must be a single value between 0 and 1")
-
-    out <- summary(hs.gauss)
-    expect_is(out, "matrix")
-    expect_equal(colnames(out),
-                 c("mean", "sd", "2.5%", "97.5%", "n_eff", "Rhat"))
-    expect_equal(nrow(out),
-                 P + 1 + 1) # intercept and extra factor level for X1
-
-    expect_equal(summary(hs.gauss, max.rows=0), out)
-    expect_equal(nrow(summary(hs.gauss, max.rows=5)), 5)
-
-    out <- summary(hs.gauss, sort="n_eff", decreasing=FALSE)
-    expect_true(all(diff(out[, "n_eff"]) > 0))
-})
-
-test_that("print.hsstan",
-{
-    expect_output(print(hs.gauss))
 })
 
 test_that("sampler.stats",
